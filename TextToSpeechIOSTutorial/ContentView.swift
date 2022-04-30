6e3d10fb57a9e4ee96e08570710c12995990fe6c
Chelsea Farley <chelsea@tripwiretech.com>
2022-04-30 17:44:19 +1200

    Text to Speech

--- TextToSpeechIOSTutorial/ContentView.swift
+++ TextToSpeechIOSTutorial/ContentView.swift
@@ -6,11 +6,24 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    @State private var name : String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Name", text: $name)
+                .padding()
+            
+            Button("Greet") {
+                let utterance = AVSpeechUtterance(string: "Hello \(name)")
+                utterance.voice = AVSpeechSynthesisVoice(language: "en-AU")
+                utterance.rate = 0.3
+                
+                let synthesizer = AVSpeechSynthesizer()
+                synthesizer.speak(utterance)
+            }
+        }
     }
 }
 
