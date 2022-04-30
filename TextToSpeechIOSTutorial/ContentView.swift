cb2cdb7d940e2e7d8e95d632b98252116da7d353
Chelsea Farley <chelsea@tripwiretech.com>
2022-04-30 17:32:33 +1200

    Initial Commit

---
+++ TextToSpeechIOSTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextToSpeechIOSTutorial
+//
+//  Created by Charles Suddens on 30/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextToSpeechIOSTutorial/TextToSpeechIOSTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToSpeechIOSTutorialApp.swift
+//  TextToSpeechIOSTutorial
+//
+//  Created by Charles Suddens on 30/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TextToSpeechIOSTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
